0380791eeef40ff7ce30a3334b350e90438bd352
sneaky-design <85828142+sneaky-design@users.noreply.github.com>
2022-07-03 11:03:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+img ="";
+objects = "";
+status = "";
+
+function preload(){
+    img = loadImage("dog_cat.jpg")
+}
+
+function ssetup(){
+    canvas = createCanvas(640 , 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "status = detecting objects";
+}
+
+function modelLoaded(){
+    console.log("model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error,result){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(img ,0 ,0 ,640 ,420 );
+
+
+    if (status != ""){
+        for (i = 0 , i<objects.length; i++ ;){
+            document.getElementById("status").innerHTML = "status =  object detected.";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" , object[i].x + 15 , objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(object[i].x, objects[i].y , objects[i].width, objects[i].height);
+        }
+    }
+}
